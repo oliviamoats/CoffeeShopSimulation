89d855bd8a813af66e453e132eabc0debc559108
oliviamoats <olivia.moats7@gmail.com>
2025-11-12 18:40:45 -0500

    Delete Coffee Shop Simulation directory

--- Coffee Shop Simulation/src/coffeeshop/simulation/CoffeeShopApp.java
+++
@@ -1,8 +0,0 @@
-package coffeeshop.simulation;
-import javafx.application.Application;
-
-public class CoffeeShopApp {
-    public static void main (String[] args) {
-        Application.launch(CoffeeShopSim.class, args);
-    }
-}
